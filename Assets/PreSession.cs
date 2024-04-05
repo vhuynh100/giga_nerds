2549322bcb9e19ee91d8216f93a667e58561b6ed
q-ariana <124922693+q-ariana@users.noreply.github.com>
2024-04-04 19:45:47 -0500

    feat: add ChatGPT Suggestions UI prefab, update PreSessionMenu prefab
    scaled up previous menus for more clarity

--- Assets/PreSession.cs
+++ Assets/PreSession.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+//TODO:
+// connect timer to Duration: XX:XX
+// update Goal text to the selected goal
 
 public class PreSession : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject Menu1;
     public GameObject Menu2;
+    public GameObject EndMenu;
     public GameObject selectGoalAlert;
+    public TMP_Text goalText;
 
     private bool goalSelected = false;
     private int playerGoal;
@@ -18,6 +23,7 @@ public class PreSession : MonoBehaviour
     {
         Menu1.SetActive(true);
         Menu2.SetActive(false);
+        EndMenu.SetActive(false);
 
     }
 
@@ -44,21 +50,32 @@ public class PreSession : MonoBehaviour
             Menu1.SetActive(false);
             Menu2.SetActive(true);
             Debug.Log("Goal selected: " + playerGoal);
+            if (playerGoal == 1) { goalText.text = "Pronunciation"; }
+            else if (playerGoal == 2) { goalText.text = "Vocabulary"; }
+            else if (playerGoal == 3) { goalText.text = "Fluency"; }
+            else if (playerGoal == 4) { goalText.text = "Grammar Accuracy"; }
+            goalText.text += " goal completed!";
         }
         else
         {
             Debug.Log("Goal not selected");
             selectGoalAlert.SetActive(true);
-            // You can display a message to the user indicating they need to select an option before continuing
         }
     }
 
     public void joinRoom()
     {
         Menu2.SetActive(false);
-
+        // start timer for session duration
         // Insert scene change code here
+        EndMenu.SetActive(true);
     }
+
+    public void returnMenu()
+    {
+        EndMenu.SetActive(false);
+    }
+
 
     // Update is called once per frame
     void Update()
