bf961a30848a4b96173ae013e22c0cd72880dda7
q-ariana <124922693+q-ariana@users.noreply.github.com>
2024-04-16 18:55:23 -0500

    feat: added Timer prefab
    updated script to properly change the timer text
    replaced art with larger pngs
    added to script to show/hide timer presets after clicking timer button

--- Assets/_Scripts/TimerDisplay.cs
+++ Assets/_Scripts/TimerDisplay.cs
@@ -1,40 +1,56 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class CountdownTimer : MonoBehaviour
 {
-    public Text timerText;
-    private float TimerCounting = 500f;
+    public TMP_Text timerText;
+    public GameObject Options;
+    private float TimerCounting = 0f;
+    private bool isTimerOn = false;
+    private bool toggleOptions = false;
 
     void Update()
     {
-        if (TimerCounting > 0)
+        if (TimerCounting > 0 && isTimerOn)
         {
             TimerCounting -= Time.deltaTime;
             UpdateTimerText();
         }
+        else if (TimerCounting <= 0 && isTimerOn)
+        { 
+            isTimerOn = false;
+            timerText.text = "Times Up!"; 
+        }
+    }
+    private void Start()
+    {
+        timerText.text = "Set Timer";
     }
 
     public void SetTimer(float minutes)
     {
+        isTimerOn = true;
         TimerCounting = minutes * 60; // Convert minutes to seconds
         UpdateTimerText();
     }
+    public void showOptions()
+    {
+        toggleOptions = !toggleOptions;
+        Options.SetActive(toggleOptions);
+    }
+
 
     private void UpdateTimerText()
     {
         int minutes = Mathf.FloorToInt(TimerCounting / 60);
         int seconds = Mathf.FloorToInt(TimerCounting % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if(seconds >= 0 && minutes >= 0)
+        {
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        }
     }
 
-    // Methods linked to buttons
-    public void SetFiveMinutes()
-    {
-        Debug.Log("button got clickd");
-        SetTimer(5f);
-    }
-    public void SetTenMinutes() => SetTimer(10f);
-    public void SetFifteenMinutes() => SetTimer(15f);
 }
